ee2e57b73972c4716379ab01511b9e691a221ecc
ArtyomDey <a.dey@mail.ru>
2022-08-23 11:31:18 +0300

    Всё готова для пуша на гитхаб

---
+++ Tak38/Program.cs
@@ -0,0 +1,24 @@
+﻿// ===========================Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+Console.Clear();
+int[] array = GetArray(5, 1, 100);
+int max = 1;
+int min = 100;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+}
+int res = max - min;
+Console.Write(String.Join(" ", array) + $" -> {res}");
---
+++ Task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// ============================Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue) //1 аргумент - размер массива, 2 - минимальное значение, 3 - максимальное значение.
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+Console.Clear();
+int[] newArray = GetArray(5, 100, 999);
+int count = 0;
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i] % 2 ==0)
+    count++;
+}
+Console.WriteLine(String.Join(" ", newArray) + $" -> {count}");
